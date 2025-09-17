f7dde35f34c06844d56dacd9d7f9634ecdb79dce
aethera <aethera.johnny@gmail.com>
2025-09-18 05:38:24 +0900

    add dbcontext and entity

---
+++ src/AuthService/Contexts/AetheraDbContext.cs
@@ -0,0 +1,33 @@
+﻿using AuthService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Contexts
+{
+    public class AetheraDbContext : DbContext
+    {
+        public AetheraDbContext()
+        {
+
+        }
+
+        public AetheraDbContext(DbContextOptions<AetheraDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Tb_User_Info>(entity =>
+            {
+                entity.Property(e => e.CreatedDatetime)
+                    .HasDefaultValueSql("now()");
+
+                entity.Property(e => e.UpdatedDatetime)
+                    .ValueGeneratedOnAddOrUpdate();
+            });
+        }
+
+        public DbSet<Tb_User_Info> Users { get; set; }
+    }
+}
