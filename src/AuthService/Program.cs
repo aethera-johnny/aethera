824480a9d15d1fac78138fb810d2bd76e87a89f1
aethera <aethera.johnny@gmail.com>
2025-09-18 16:55:32 +0900

    add migration and update

--- src/AuthService/Program.cs
+++ src/AuthService/Program.cs
@@ -1,3 +1,6 @@
+using AuthService.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace AuthService
 {
@@ -12,6 +15,18 @@ namespace AuthService
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+            builder.Services.AddDbContext<AetheraDbContext>(options =>
+            {
+                options.ConfigureWarnings(warnings => warnings.Log(CoreEventId.NavigationBaseIncludeIgnored));
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+                                  builder =>
+                                  {
+                                      builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+                                      builder.CommandTimeout(60);
+                                  });
+                options.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddFilter((category, level) => level >= LogLevel.Warning)));
+            }, ServiceLifetime.Scoped);
 
             var app = builder.Build();
 
